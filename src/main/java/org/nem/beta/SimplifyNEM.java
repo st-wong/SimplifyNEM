32e14afecf01797168d25856ab90388b4e656b6d
Shin Tatt, Wong <st-wong@users.noreply.github.com>
2017-07-05 17:26:24 +0800

    Updated to do show case basic NEM functionalities
    - Transaction for NEM and Mosaics, Rent Namespace and Create Mosaic.
    - Get existing Transactions for account
    - Include Web Socket example

---
+++ src/main/java/org/nem/beta/SimplifyNEM.java
@@ -0,0 +1,232 @@
+package org.nem.beta;
+
+import org.nem.beta.utility.UtilityFunctions;
+import org.nem.beta.model.WSMonitorIncomingHandler;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.*;
+import java.util.function.Supplier;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+import org.bouncycastle.pqc.math.linearalgebra.ByteUtils;
+
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import org.nem.core.crypto.KeyPair;
+import org.nem.core.crypto.PrivateKey;
+import org.nem.core.messages.PlainMessage;
+import org.nem.core.messages.SecureMessage;
+import org.nem.core.model.primitive.Amount;
+import org.nem.core.model.primitive.BlockHeight;
+import org.nem.core.model.primitive.Quantity;
+import org.nem.core.serialization.BinarySerializer;
+import org.nem.core.time.SystemTimeProvider;
+import org.nem.core.time.TimeInstant;
+import org.nem.core.model.*;
+import org.nem.core.model.mosaic.*;
+import org.nem.core.model.namespace.*;
+
+public class SimplifyNEM {
+
+    //Global Variables
+    public static UtilityFunctions utilityFunc;
+    public static final String NEM_ADDRESS = "http://bob.nem.ninja:7890";
+    public static final String WSNEM_ADDRESS = "http://104.128.226.60:7778";
+    public static final String API_ANNOUNCE_TRANSACTION = "/transaction/announce";
+    public static final String API_ALL_TRANSACTIONS = "/account/transfers/all";
+    public static final String API_UNCONFIRMED_TRANSACTIONS = "/account/unconfirmedTransactions";
+    public static final String TESTNET_NAMESPACE_SINKING = "TAMESPACEWH4MKFMBCVFERDPOOP4FK7MTDJEYP35";
+    public static final String TESTNET_MOSAIC_SINKING = "TBMOSAICOD4F54EE5CDMR23CCBGOAM2XSJBR5OLC";
+    public static final String TESTNET_APOSTILLE_SINKING = "TC7MCY5AGJQXZQ4BN3BOPNXUVIGDJCOHBPGUM2GE";
+    public static final String APOSTILLE_TEST_FILE = "C:\\Users\\shint\\OneDrive\\Documents\\Projects\\myXEM\\SimplifyNEM\\testNashorn.txt";
+    public static final long MOSAIC_MAX_QUANTITY = 9_000_000_000_000_000L;
+
+    public static void main(String[] args) {
+        utilityFunc = new UtilityFunctions(NEM_ADDRESS);
+
+        //Send Message & XEM
+        //String results = SendTransaction(senderPrivateKey, receiverAddress, 1000, "this is a test message, it should be more than 32 bytes", null, 0);
+        //System.out.println(String.format("Results: %s", results));
+
+        //Rent new namespace
+        //String results = RentNamespace(senderPrivateKey, "artisancof", null);
+        //System.out.println(String.format("Results: %s", results));
+        //results = RentNamespace(senderPrivateKey, "art", "artisancof");
+        //System.out.println(String.format("Results: %s", results));
+
+        //New mosaic definition
+        //String results = CreateMosaic(senderPrivateKey, "artisancof.art:credits", "points", "100000");
+        //System.out.println(String.format("Results: %s", results));
+
+        //Transfer mosaic
+        //String results = SendTransaction(senderPrivateKey, receiverAddress, 0,"sending mosiacs", "artisancof.art:credits", 1);
+        //System.out.println(String.format("Results: %s", results));
+
+        //Get existing transactions made
+        GetAllConfirmedMessages(senderAddress);
+        StartWebSocketService(senderAddress);
+
+        utilityFunc.Disconnect();
+    }
+
+    private static String SendTransaction(String senderPrivateKey, String receiverAddress, long amount, String message, String mosaicId, long mosaicQty) {
+        TimeInstant timeInstant = new SystemTimeProvider().getCurrentTime();
+        Account senderAccount = new Account(new KeyPair(PrivateKey.fromHexString(senderPrivateKey)));
+        Account recipientAccount = new Account(Address.fromEncoded(receiverAddress));
+        TransferTransactionAttachment attachment = (message == null && mosaicId == null && mosaicQty != 0) ? null : new TransferTransactionAttachment();
+
+        if (mosaicId != null && mosaicQty != 0) {
+            MosaicId mosyId = MosaicId.parse(mosaicId);
+            MosaicFeeInformation mosaicFeeInformation = utilityFunc.findById(mosyId);
+            Double mosaicQuantityDouble = Double.valueOf(mosaicQty) * Math.pow(10, mosaicFeeInformation.getDivisibility());
+            attachment.addMosaic(mosyId, Quantity.fromValue(mosaicQuantityDouble.longValue()));
+        }
+
+        if (message != null) {
+            //PlainMessage for non encrypted messages, SecureMessage for encrypted messages. Fees calculated after encryption
+            PlainMessage PM = new PlainMessage(message.getBytes());
+            attachment.setMessage(PM);
+        }
+
+        TransferTransaction transaction = new TransferTransaction(timeInstant, senderAccount, recipientAccount, Amount.fromNem(amount), attachment);
+        //transaction.setFee(new DefaultTransactionFeeCalculator(utilityFunc, () -> BlockHeight.ONE, BlockHeight.MAX).calculateMinimumFee(transaction));
+        transaction.setFee(new TransactionFeeCalculatorAfterFork(utilityFunc).calculateMinimumFee(transaction));
+        transaction.setDeadline(timeInstant.addHours(23));
+        transaction.sign();
+
+        JSONObject paramsJSON = new JSONObject();
+        final byte[] data = BinarySerializer.serializeToBytes(transaction.asNonVerifiable());
+        paramsJSON.put("data", ByteUtils.toHexString(data));
+        paramsJSON.put("signature", transaction.getSignature().toString());
+        return utilityFunc.PostResults(API_ANNOUNCE_TRANSACTION, paramsJSON);
+    }
+
+    private static String RentNamespace(String accountPrivateKey, String namespaceToRent, String parentNamespace) {
+        TimeInstant timeInstant = new SystemTimeProvider().getCurrentTime();
+        Account namespaceAccount = new Account(new KeyPair(PrivateKey.fromHexString(accountPrivateKey)));
+        Account sinkingfeeAccount = new Account(Address.fromEncoded(TESTNET_NAMESPACE_SINKING));
+
+        ProvisionNamespaceTransaction namespaceTransaction = new ProvisionNamespaceTransaction(timeInstant, namespaceAccount, sinkingfeeAccount, (parentNamespace == null) ? Amount.fromNem(5000) : Amount.fromNem(200), new NamespaceIdPart(namespaceToRent), (parentNamespace == null) ? null : new NamespaceId(parentNamespace));
+        namespaceTransaction.setFee(Amount.fromNem(20));
+        namespaceTransaction.setDeadline(timeInstant.addHours(23));
+        namespaceTransaction.sign();
+
+        JSONObject paramsJSON = new JSONObject();
+        final byte[] data = BinarySerializer.serializeToBytes(namespaceTransaction.asNonVerifiable());
+        paramsJSON.put("data", ByteUtils.toHexString(data));
+        paramsJSON.put("signature", namespaceTransaction.getSignature().toString());
+        return utilityFunc.PostResults(API_ANNOUNCE_TRANSACTION, paramsJSON);
+
+    }
+
+    private static String CreateMosaic(String accountPrivateKey, String mosaicName, String mosaicDescription, String mosaicQty) {
+        TimeInstant timeInstant = new SystemTimeProvider().getCurrentTime();
+        Account mosaicAccount = new Account(new KeyPair(PrivateKey.fromHexString(accountPrivateKey)));
+        Properties mosaicProperty = new Properties();
+        mosaicProperty.put("divisibility", "0");
+        mosaicProperty.put("initialSupply", mosaicQty);
+        mosaicProperty.put("supplyMutable", "false");
+        mosaicProperty.put("transferable", "true");
+        MosaicDefinition mosaicDef = new MosaicDefinition(mosaicAccount, MosaicId.parse(mosaicName), new MosaicDescriptor(mosaicDescription), new DefaultMosaicProperties(mosaicProperty),  new MosaicLevy(MosaicTransferFeeType.Absolute, mosaicAccount, MosaicId.parse("nem:xem"), Quantity.ZERO));
+        System.out.println(mosaicDef.getProperties().getInitialSupply());
+        System.out.println(mosaicProperty.getProperty("initialSupply"));
+        System.out.println(mosaicQty);
+
+        MosaicDefinitionCreationTransaction mosaicTransaction = new MosaicDefinitionCreationTransaction(timeInstant, mosaicAccount, mosaicDef);
+        mosaicTransaction.setFee(Amount.fromNem(20));
+        mosaicTransaction.setDeadline(timeInstant.addHours(23));
+        mosaicTransaction.sign();
+
+        JSONObject paramsJSON = new JSONObject();
+        final byte[] data = BinarySerializer.serializeToBytes(mosaicTransaction.asNonVerifiable());
+        paramsJSON.put("data", ByteUtils.toHexString(data));
+        paramsJSON.put("signature", mosaicTransaction.getSignature().toString());
+        return utilityFunc.PostResults(API_ANNOUNCE_TRANSACTION, paramsJSON);
+    }
+
+    private static String EncryptedMessage(String ownerPrivateKey, String fileLocation)
+    {
+        File apostileFile = new File(fileLocation);
+        byte[] apostileFileBytes = new byte[(int) apostileFile.length()];
+        try {
+            FileInputStream fileStream = new FileInputStream(apostileFile);
+            fileStream.read(apostileFileBytes);
+            fileStream.close();
+        } catch (Exception e) {
+            System.out.println(String.format("Exception caught: %s", e.getMessage()));
+            e.printStackTrace();
+        }
+
+        TimeInstant timeInstant = new SystemTimeProvider().getCurrentTime();
+        Account ownerAccount = new Account(new KeyPair(PrivateKey.fromHexString(ownerPrivateKey)));
+        Account sinkingfeeAccount = new Account(Address.fromEncoded(TESTNET_APOSTILLE_SINKING));
+        TransferTransactionAttachment attachment = new TransferTransactionAttachment();
+        SecureMessage encryptMessage = SecureMessage.fromEncodedPayload(ownerAccount, sinkingfeeAccount, apostileFileBytes);
+        attachment.setMessage(encryptMessage);
+
+        TransferTransaction apostilleTransaction = new TransferTransaction(timeInstant, ownerAccount, sinkingfeeAccount, Amount.ZERO, attachment);
+        apostilleTransaction.setFee(new DefaultTransactionFeeCalculator(null, () -> BlockHeight.ONE, BlockHeight.MAX).calculateMinimumFee(apostilleTransaction));
+        apostilleTransaction.setDeadline(timeInstant.addHours(23));
+        apostilleTransaction.sign();
+
+        JSONObject paramsJSON = new JSONObject();
+        final byte[] data = BinarySerializer.serializeToBytes(apostilleTransaction.asNonVerifiable());
+        paramsJSON.put("data", ByteUtils.toHexString(data));
+        paramsJSON.put("signature", apostilleTransaction.getSignature().toString());
+        return utilityFunc.PostResults(API_ANNOUNCE_TRANSACTION, paramsJSON);
+    }
+
+    private static void GetAllConfirmedMessages(String addressToMonitor) {
+        System.out.println("\nAll Confirmed Transactions");
+        JSONArray resultJSONArr = null;
+        JSONObject transactionJSON = null;
+        long currTransactionID = 0;
+        long lastTransactionID = 0;
+
+        do {
+            lastTransactionID = currTransactionID;
+            String result = utilityFunc.GetResults(API_ALL_TRANSACTIONS, "address=" + addressToMonitor + ((lastTransactionID == 0) ? "" : "&id=" + lastTransactionID));
+            resultJSONArr = JSONObject.fromObject(result).getJSONArray("data");
+            for (int i = 0; i < resultJSONArr.size(); ++i) {
+                transactionJSON = resultJSONArr.getJSONObject(i).getJSONObject("transaction");
+
+                //Show only transactions with messages
+                if (transactionJSON.containsKey("message")) {
+                    utilityFunc.DecodeMessage(transactionJSON);
+                    currTransactionID = resultJSONArr.getJSONObject(i).getJSONObject("meta").getLong("id");
+                }
+            }
+        } while (lastTransactionID != currTransactionID);
+    }
+
+    private static void StartWebSocketService(String addressToMonitor) {
+        //Web Socket monitor incoming messages in block chain
+        List<Transport> transports = new ArrayList<Transport>();
+        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+        SockJsClient sockJsClient = new SockJsClient(transports);
+        WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+        stompClient.setMessageConverter(new StringMessageConverter());
+        StompSessionHandler handler = new WSMonitorIncomingHandler(addressToMonitor);
+        stompClient.connect(WSNEM_ADDRESS + "/w/messages", handler);
+        System.out.println("Listening to messages...");
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+            while (!scanner.nextLine().equalsIgnoreCase("stop")) {}
+        } catch (Exception e) {
+            System.out.println(String.format("Exception caught: %s", e.getMessage()));
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+            stompClient.stop();
+        }
+    }
+}
---
+++ src/main/java/org/nem/beta/utility/UtilityFunctions.java
@@ -0,0 +1,150 @@
+package org.nem.beta.utility;
+
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.*;
+import org.apache.http.util.EntityUtils;
+
+import org.nem.core.crypto.PublicKey;
+import org.nem.core.model.Address;
+import org.nem.core.model.mosaic.MosaicFeeInformation;
+import org.nem.core.model.mosaic.MosaicFeeInformationLookup;
+import org.nem.core.model.mosaic.MosaicId;
+import org.nem.core.model.primitive.Amount;
+import org.nem.core.model.primitive.Supply;
+import org.nem.core.time.SystemTimeProvider;
+
+import org.bouncycastle.pqc.math.linearalgebra.ByteUtils;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+
+public class UtilityFunctions implements MosaicFeeInformationLookup {
+
+    private static CloseableHttpClient httpClient;
+    private static String urlNode;
+    private static final String API_MOSAIC_DEFINITION_PAGE = "/namespace/mosaic/definition/page";
+
+    public UtilityFunctions(String url) {
+        urlNode = url;
+
+        try {
+            httpClient = HttpClients.createDefault();
+        } catch (Exception e) {
+            System.out.println(String.format("Exception caught: %s", e.getMessage()));
+            e.printStackTrace();
+        }
+    }
+
+    public static void Disconnect() {
+        try {
+            httpClient.close();
+        } catch (Exception e) {
+            System.out.println(String.format("Exception caught: %s", e.getMessage()));
+            e.printStackTrace();
+        }
+    }
+
+    //Present results in a more readable manner
+    public static void DecodeMessage(JSONObject transactionJSON) {
+        StringBuilder outMessage = new StringBuilder();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        outMessage.append(String.format("%nSender Address: %s", Address.fromPublicKey(PublicKey.fromHexString(transactionJSON.getString("signer"))).toString()));
+        outMessage.append(String.format("%nTransaction Amount: %d XEM", Amount.fromMicroNem(transactionJSON.getLong("amount")).getNumNem()));
+        outMessage.append(String.format("%nTimestamp: %s", dateFormat.format(new Date((transactionJSON.getLong("timeStamp") * 1000) + SystemTimeProvider.getEpochTimeMillis()))));
+
+        // Get Non encrypted message
+        if (transactionJSON.getJSONObject("message").containsKey("type") && transactionJSON.getJSONObject("message").getInt("type") == 1) {
+            JSONObject messageJSON = transactionJSON.getJSONObject("message");
+            outMessage.append(String.format("%nMessage: %s", new String(ByteUtils.fromHexString(messageJSON.getString("payload")))));
+        } else {
+            outMessage.append(String.format("%nNo Message"));
+        }
+
+        System.out.println(outMessage.toString());
+    }
+
+    //HTTP Post call
+    public static String PostResults(String APItoCall, JSONObject paramsJSON) {
+        CloseableHttpResponse response;
+        String result = "";
+
+        try {
+            HttpPost method = new HttpPost(urlNode + APItoCall);
+            StringEntity params = new StringEntity(paramsJSON.toString());
+            method.setHeader("content-type", "application/json");
+            method.setEntity(params);
+            response = httpClient.execute(method);
+            result = EntityUtils.toString(response.getEntity());
+        } catch (Exception e) {
+            System.out.println(String.format("Exception caught: %s", e.getMessage()));
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    //HTTP Get call
+    public static String GetResults(String APItoCall, String ParamstoPass) {
+        CloseableHttpResponse response;
+        String result = "";
+
+        try {
+            HttpGet method = new HttpGet(urlNode + APItoCall + "?" + ParamstoPass);
+            response = httpClient.execute(method);
+            result = EntityUtils.toString(response.getEntity());
+        } catch (Exception e) {
+            System.out.println(String.format("Exception caught: %s", e.getMessage()));
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    //HTTP Get Mosaic Fee Information
+    public MosaicFeeInformation findById(final MosaicId id) {
+        CloseableHttpResponse response = null;
+        String result = "";
+
+        try {
+            HttpGet method = new HttpGet(urlNode + API_MOSAIC_DEFINITION_PAGE + "?namespace=" + id.getNamespaceId().toString());
+            response = httpClient.execute(method);
+            result = EntityUtils.toString(response.getEntity());
+        } catch (Exception e) {
+            System.out.println(String.format("Exception caught: %s", e.getMessage()));
+            e.printStackTrace();
+        }
+
+        JSONArray jsonArr = JSONObject.fromObject(result).getJSONArray("data");
+
+        for (int i = 0; i < jsonArr.size(); i++) {
+            JSONObject mosaic = jsonArr.getJSONObject(i).getJSONObject("mosaic");
+
+            if (id.getName().equals(jsonArr.getJSONObject(i).getJSONObject("mosaic").getJSONObject("id").getString("name"))) {
+                JSONArray properties = mosaic.getJSONArray("properties");
+                String initialSupply = "";
+                String divisibility = "";
+
+                for (int j = 0; j < properties.size(); j++) {
+                    JSONObject property = properties.getJSONObject(j);
+
+                    if ("initialSupply".equals(property.getString("name"))) {
+                        initialSupply = property.getString("value");
+                    } else if ("divisibility".equals(property.getString("name"))) {
+                        divisibility = property.getString("value");
+                    }
+                }
+
+                if (!"".equals(initialSupply) && !"".equals(divisibility)) {
+                    return new MosaicFeeInformation(Supply.fromValue(Long.valueOf(initialSupply)), Integer.valueOf(divisibility));
+                }
+            }
+        }
+
+        return null;
+    }
+}
