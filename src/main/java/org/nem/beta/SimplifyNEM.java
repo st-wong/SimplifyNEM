f4cd956395e9dce32fb5a20e41c913acf530fa51
Shin Tatt, Wong <st-wong@users.noreply.github.com>
2018-01-11 22:14:37 +0800

    Add files via upload
    - Updated fee calculator

--- src/main/java/org/nem/beta/SimplifyNEM.java
+++ src/main/java/org/nem/beta/SimplifyNEM.java
@@ -11,6 +11,7 @@ import net.sf.json.JSONObject;
 import net.sf.json.JSONArray;
 import org.bouncycastle.pqc.math.linearalgebra.ByteUtils;
 
+import org.nem.core.model.primitive.Supply;
 import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
@@ -26,7 +27,6 @@ import org.nem.core.crypto.PrivateKey;
 import org.nem.core.messages.PlainMessage;
 import org.nem.core.messages.SecureMessage;
 import org.nem.core.model.primitive.Amount;
-import org.nem.core.model.primitive.BlockHeight;
 import org.nem.core.model.primitive.Quantity;
 import org.nem.core.serialization.BinarySerializer;
 import org.nem.core.time.SystemTimeProvider;
@@ -39,43 +39,48 @@ public class SimplifyNEM {
 
     //Global Variables
     public static UtilityFunctions utilityFunc;
-    public static final String NEM_ADDRESS = "http://104.128.226.60:7890";
+    public static final String NEM_ADDRESS = "http://23.228.67.85:7890";
     public static final String WSNEM_ADDRESS = "http://104.128.226.60:7778";
     public static final String API_ANNOUNCE_TRANSACTION = "/transaction/announce";
     public static final String API_ALL_TRANSACTIONS = "/account/transfers/all";
     public static final String API_UNCONFIRMED_TRANSACTIONS = "/account/unconfirmedTransactions";
-    public static final String TESTNET_NAMESPACE_SINKING = "TAMESPACEWH4MKFMBCVFERDPOOP4FK7MTDJEYP35";
-    public static final String TESTNET_MOSAIC_SINKING = "TBMOSAICOD4F54EE5CDMR23CCBGOAM2XSJBR5OLC";
     public static final String TESTNET_APOSTILLE_SINKING = "TC7MCY5AGJQXZQ4BN3BOPNXUVIGDJCOHBPGUM2GE";
     public static final long MOSAIC_MAX_QUANTITY = 9_000_000_000_000_000L;
 
     public static void main(String[] args) {
+        NetworkInfos.setDefault(NetworkInfos.getTestNetworkInfo());
         utilityFunc = new UtilityFunctions(NEM_ADDRESS);
 
         //Send Message and/or XEM
-        //String results = SendTransaction(senderPrivateKey, receiverAddress, 1000, "this is a test message, it should be more than 32 bytes", null, 0);
+        //String results = SendTransaction(senderPrivateKey, receiverAddress, 1000L, messageHere, null);
         //System.out.println(String.format("Results: %s", results));
 
         //Rent new namespace
-        //String results = RentNamespace(senderPrivateKey, "coffee", null);
+        //String results = RentNamespace(senderPrivateKey, "rootName", null);
         //System.out.println(String.format("Results: %s", results));
-        //results = RentNamespace(senderPrivateKey, "columbia", "coffee");
+        //results = RentNamespace(senderPrivateKey, "rootName", "subName");
         //System.out.println(String.format("Results: %s", results));
 
         //New mosaic definition
-        //String results = CreateMosaic(senderPrivateKey, "coffee.columbia:caffeine", "points", "100000");
+        //String results = CreateMosaic(senderPrivateKey, "rootName.subName:mosaicName", "MosaicDescription", 1_000_000L);
         //System.out.println(String.format("Results: %s", results));
+
+        //Mosaic Supply Change
+        //String results = EditMosaic(senderPrivateKey, mosaicID, mosaicQtyToChange, TOcreateORdestroy);
+        //System.out.println(String.format("Results %s", results));
 
         //Transfer mosaic
-        //String results = SendTransaction(senderPrivateKey, receiverAddress, 0,"sending mosaics", "coffee.columbia:caffeine", 1);
+        //HashMap<String, Long> mosaicsToSend = new HashMap<String, Long>();
+        //mosaicsToSend.put(mosaidID, Long.valueOf(mosaicQtyToTransfer));
+        //String results = SendTransaction(senderPrivateKey, receiverAddress, 0, custom_Message, mosaicsToSend);
         //System.out.println(String.format("Results: %s", results));
 
-        //Send Encrypted Message, use file to simulate encrypted data
-        //String results = SendEncryptedMessage(senderPrivateKey, receiverAddress, "LocationToFile\\sample.txt");
+        //Send Hex/Encrypted Message, use file to simulate data
+        //String results = SendEncryptedMessage(senderPrivateKey, receiverAddress, "LocationToFile\\sample.txt", true);
         //System.out.println(String.format("Results: %s", results));
 
         //Create MultiSig Account, MultiSig Account must have sufficient XEM to perform this operation hence senderPrivateKey is to feed the account with funds
-        //String results = CreateMultisigAcc(3,2, senderPrivateKey);
+        //String results = CreateMultiSigAcc(3,2, senderPrivateKey);
         //System.out.println(String.format("Results: %s", results));
 
         //Start a MultiSig Transaction, get Account details from CreateMultisigAcc() outputs
@@ -95,17 +100,22 @@ public class SimplifyNEM {
         utilityFunc.Disconnect();
     }
 
-    private static String SendTransaction(String senderPrivateKey, String receiverAddress, long amount, String message, String mosaicId, long mosaicQty) {
+    private static String SendTransaction(String senderPrivateKey, String receiverAddress, long amount, String message, HashMap<String, Long> mosaicsToSend) {
         TimeInstant timeStamp = new SystemTimeProvider().getCurrentTime();
         Account senderAccount = new Account(new KeyPair(PrivateKey.fromHexString(senderPrivateKey)));
         Account recipientAccount = new Account(Address.fromEncoded(receiverAddress));
-        TransferTransactionAttachment attachment = (message == null && mosaicId == null && mosaicQty != 0) ? null : new TransferTransactionAttachment();
+        TransferTransactionAttachment attachment = (message == null && mosaicsToSend == null) ? null : new TransferTransactionAttachment();
 
-        if (mosaicId != null && mosaicQty != 0) {
-            MosaicId mosyId = MosaicId.parse(mosaicId);
-            MosaicFeeInformation mosaicFeeInformation = utilityFunc.findById(mosyId);
-            Double mosaicQuantityDouble = Double.valueOf(mosaicQty) * Math.pow(10, mosaicFeeInformation.getDivisibility());
-            attachment.addMosaic(mosyId, Quantity.fromValue(mosaicQuantityDouble.longValue()));
+        if (mosaicsToSend != null) {
+            for(Map.Entry mosy:mosaicsToSend.entrySet()) {
+                MosaicId mosyId = MosaicId.parse((String)mosy.getKey());
+                if (mosyId.toString().equals("nem:xem")) {
+                    attachment.addMosaic(mosyId, Quantity.fromValue((Long)mosy.getValue() * (long) Math.pow(10, 6)));
+                } else {
+                    MosaicFeeInformation mosaicFeeInformation = utilityFunc.findById(mosyId);
+                    attachment.addMosaic(mosyId, Quantity.fromValue((Long) mosy.getValue() * (long) Math.pow(10, mosaicFeeInformation.getDivisibility())));
+                }
+            }
         }
 
         if (message != null) {
@@ -115,9 +125,7 @@ public class SimplifyNEM {
         }
 
         TransferTransaction transaction = new TransferTransaction(timeStamp, senderAccount, recipientAccount, Amount.fromNem(amount), attachment);
-        //572500, 975000 Testnet Fee Fork Heights
-        //875000, 1110000 Mainnet Fee Fork Heights
-        transaction.setFee(new DefaultTransactionFeeCalculator(utilityFunc, () -> new BlockHeight(utilityFunc.getBlockHeight()), new BlockHeight(975000)).calculateMinimumFee(transaction));
+        transaction.setFee(utilityFunc.TransactionFeeCalculator.calculateMinimumFee(transaction));
         transaction.setDeadline(timeStamp.addHours(23));
         transaction.sign();
 
@@ -131,10 +139,9 @@ public class SimplifyNEM {
     private static String RentNamespace(String accountPrivateKey, String namespaceToRent, String parentNamespace) {
         TimeInstant timeStamp = new SystemTimeProvider().getCurrentTime();
         Account namespaceAccount = new Account(new KeyPair(PrivateKey.fromHexString(accountPrivateKey)));
-        Account sinkingfeeAccount = new Account(Address.fromEncoded(TESTNET_NAMESPACE_SINKING));
 
-        ProvisionNamespaceTransaction namespaceTransaction = new ProvisionNamespaceTransaction(timeStamp, namespaceAccount, sinkingfeeAccount, (parentNamespace == null) ? Amount.fromNem(100) : Amount.fromNem(10), new NamespaceIdPart(namespaceToRent), (parentNamespace == null) ? null : new NamespaceId(parentNamespace));
-        namespaceTransaction.setFee(Amount.fromMicroNem(new Double(0.15 * Amount.MICRONEMS_IN_NEM).longValue()));
+        ProvisionNamespaceTransaction namespaceTransaction = new ProvisionNamespaceTransaction(timeStamp, namespaceAccount, MosaicConstants.NAMESPACE_OWNER_NEM, (parentNamespace == null) ? Amount.fromNem(100) : Amount.fromNem(10), new NamespaceIdPart(namespaceToRent), (parentNamespace == null) ? null : new NamespaceId(parentNamespace));
+        namespaceTransaction.setFee(Amount.fromMicroNem((long)(0.15 * Amount.MICRONEMS_IN_NEM)));
         namespaceTransaction.setDeadline(timeStamp.addHours(23));
         namespaceTransaction.sign();
 
@@ -146,19 +153,19 @@ public class SimplifyNEM {
 
     }
 
-    private static String CreateMosaic(String accountPrivateKey, String mosaicName, String mosaicDescription, String mosaicQty) {
+    private static String CreateMosaic(String accountPrivateKey, String mosaicName, String mosaicDescription, long mosaicQty) {
         TimeInstant timeStamp = new SystemTimeProvider().getCurrentTime();
         Account mosaicAccount = new Account(new KeyPair(PrivateKey.fromHexString(accountPrivateKey)));
 
         Properties mosaicProperty = new Properties();
         mosaicProperty.put("divisibility", "0");
-        mosaicProperty.put("initialSupply", mosaicQty);
+        mosaicProperty.put("initialSupply", Long.toString(mosaicQty));
         mosaicProperty.put("supplyMutable", "false");
         mosaicProperty.put("transferable", "true");
         MosaicDefinition mosaicDef = new MosaicDefinition(mosaicAccount, MosaicId.parse(mosaicName), new MosaicDescriptor(mosaicDescription), new DefaultMosaicProperties(mosaicProperty), new MosaicLevy(MosaicTransferFeeType.Absolute, mosaicAccount, MosaicId.parse("nem:xem"), Quantity.ZERO));
 
-        MosaicDefinitionCreationTransaction mosaicTransaction = new MosaicDefinitionCreationTransaction(timeStamp, mosaicAccount, mosaicDef);
-        mosaicTransaction.setFee(Amount.fromMicroNem(new Double(0.15 * Amount.MICRONEMS_IN_NEM).longValue()));
+        MosaicDefinitionCreationTransaction mosaicTransaction = new MosaicDefinitionCreationTransaction(timeStamp, mosaicAccount, mosaicDef, MosaicConstants.MOSAIC_CREATION_FEE_SINK, Amount.fromNem(10));
+        mosaicTransaction.setFee(Amount.fromMicroNem((long)(0.15 * Amount.MICRONEMS_IN_NEM)));
         mosaicTransaction.setDeadline(timeStamp.addHours(23));
         mosaicTransaction.sign();
 
@@ -169,7 +176,24 @@ public class SimplifyNEM {
         return utilityFunc.PostResults(API_ANNOUNCE_TRANSACTION, paramsJSON);
     }
 
-    private static String SendEncryptedMessage(String senderPrivateKey, String receiverAddress, String fileLocation) {
+    private static String EditMosaic(String accountPrivateKey, String mosaicName, long mosaicQty, boolean toCreate) {
+        TimeInstant timeStamp = new SystemTimeProvider().getCurrentTime();
+        Account mosaicAccount = new Account(new KeyPair(PrivateKey.fromHexString(accountPrivateKey)));
+
+        MosaicId mosyId = MosaicId.parse(mosaicName);
+        MosaicSupplyChangeTransaction mosaicChangeTransaction = new MosaicSupplyChangeTransaction(timeStamp, mosaicAccount, mosyId, (toCreate) ? MosaicSupplyType.Create : MosaicSupplyType.Delete, Supply.fromValue(mosaicQty));
+        mosaicChangeTransaction.setFee(utilityFunc.TransactionFeeCalculator.calculateMinimumFee(mosaicChangeTransaction));
+        mosaicChangeTransaction.setDeadline(timeStamp.addHours(23));
+        mosaicChangeTransaction.sign();
+
+        JSONObject paramsJSON = new JSONObject();
+        final byte[] data = BinarySerializer.serializeToBytes(mosaicChangeTransaction.asNonVerifiable());
+        paramsJSON.put("data", ByteUtils.toHexString(data));
+        paramsJSON.put("signature", mosaicChangeTransaction.getSignature().toString());
+        return utilityFunc.PostResults(API_ANNOUNCE_TRANSACTION, paramsJSON);
+    }
+
+    private static String SendEncryptedMessage(String senderPrivateKey, String receiverAddress, String fileLocation, boolean toHex) {
         File encryptData = new File(fileLocation);
         byte[] encryptDataBytes = new byte[(int) encryptData.length()];
         try {
@@ -185,13 +209,16 @@ public class SimplifyNEM {
         Account senderAccount = new Account(new KeyPair(PrivateKey.fromHexString(senderPrivateKey)));
         Account receiverAccount = new Account(Address.fromEncoded(receiverAddress));
         TransferTransactionAttachment attachment = new TransferTransactionAttachment();
-        SecureMessage encryptMessage = SecureMessage.fromEncodedPayload(senderAccount, receiverAccount, encryptDataBytes);
-        attachment.setMessage(encryptMessage);
+        if (toHex) {
+            PlainMessage hexMessage = new PlainMessage(encryptDataBytes);
+            attachment.setMessage(hexMessage);
+        } else {
+            SecureMessage encryptMessage = SecureMessage.fromEncodedPayload(senderAccount, receiverAccount, encryptDataBytes);
+            attachment.setMessage(encryptMessage);
+        }
 
         TransferTransaction encryptTransaction = new TransferTransaction(timeStamp, senderAccount, receiverAccount, Amount.ZERO, attachment);
-        //572500, 975000 Testnet Fee Fork Heights
-        //875000, 1110000 Mainnet Fee Fork Heights
-        encryptTransaction.setFee(new DefaultTransactionFeeCalculator(utilityFunc, () -> new BlockHeight(utilityFunc.getBlockHeight()), new BlockHeight(975000)).calculateMinimumFee(encryptTransaction));
+        encryptTransaction.setFee(utilityFunc.TransactionFeeCalculator.calculateMinimumFee(encryptTransaction));
         encryptTransaction.setDeadline(timeStamp.addHours(23));
         encryptTransaction.sign();
 
@@ -202,7 +229,7 @@ public class SimplifyNEM {
         return utilityFunc.PostResults(API_ANNOUNCE_TRANSACTION, paramsJSON);
     }
 
-    private static String CreateMultisigAcc(int noSigAccount, int noSigRequired, String fundingPrivKey) {
+    private static String CreateMultiSigAcc(int noSigAccount, int noSigRequired, String fundingPrivKey) {
         KeyPair MultiSigKeys = new KeyPair();
         Account MultiSigAccount = new Account(MultiSigKeys);
 
@@ -210,7 +237,7 @@ public class SimplifyNEM {
         System.out.println(String.format("MultiSig Private Key: %s", MultiSigKeys.getPrivateKey()));
         System.out.println(String.format("MultiSig Public Key: %s", MultiSigKeys.getPublicKey()));
         System.out.println(String.format("MultiSig Address: %s", MultiSigAccount.getAddress()));
-        String result = SendTransaction(fundingPrivKey, MultiSigAccount.getAddress().toString(), 5000, null, null, 0);
+        String result = SendTransaction(fundingPrivKey, MultiSigAccount.getAddress().toString(), 5000, null, null);
         if (!result.toLowerCase().contains("success")) {
             System.out.println("Failed to populate MultiSig Account");
             return result;
@@ -229,9 +256,7 @@ public class SimplifyNEM {
 
         TimeInstant timeStamp = new SystemTimeProvider().getCurrentTime();
         MultisigAggregateModificationTransaction multisigTransaction = new MultisigAggregateModificationTransaction(timeStamp, MultiSigAccount, coSigModColl, minCosigMod);
-        //572500, 975000 Testnet Fee Fork Heights
-        //875000, 1110000 Mainnet Fee Fork Heights
-        multisigTransaction.setFee(new DefaultTransactionFeeCalculator(utilityFunc, () -> new BlockHeight(utilityFunc.getBlockHeight()), new BlockHeight(975000)).calculateMinimumFee(multisigTransaction));
+        multisigTransaction.setFee(utilityFunc.TransactionFeeCalculator.calculateMinimumFee(multisigTransaction));
         multisigTransaction.setDeadline(timeStamp.addHours(23));
         multisigTransaction.sign();
 
@@ -253,8 +278,7 @@ public class SimplifyNEM {
         if (mosaicId != null && mosaicQty != 0) {
             MosaicId mosyId = MosaicId.parse(mosaicId);
             MosaicFeeInformation mosaicFeeInformation = utilityFunc.findById(mosyId);
-            Double mosaicQuantityDouble = Double.valueOf(mosaicQty) * Math.pow(10, mosaicFeeInformation.getDivisibility());
-            attachment.addMosaic(mosyId, Quantity.fromValue(mosaicQuantityDouble.longValue()));
+            attachment.addMosaic(mosyId, Quantity.fromValue(mosaicQty * (long)Math.pow(10, mosaicFeeInformation.getDivisibility())));
         }
 
         if (message != null) {
@@ -264,14 +288,11 @@ public class SimplifyNEM {
         }
 
         TransferTransaction transaction = new TransferTransaction(timeStamp, multisigAccount, recipientAccount, Amount.fromNem(amount), attachment);
-        //572500, 975000 Testnet Fee Fork Heights
-        //875000, 1110000 Mainnet Fee Fork Heights
-        DefaultTransactionFeeCalculator feeCalculator = new DefaultTransactionFeeCalculator(utilityFunc, () -> new BlockHeight(utilityFunc.getBlockHeight()), new BlockHeight(975000));
-        transaction.setFee(feeCalculator.calculateMinimumFee(transaction));
+        transaction.setFee(utilityFunc.TransactionFeeCalculator.calculateMinimumFee(transaction));
         transaction.setDeadline(timeStamp.addHours(23));
 
         MultisigTransaction multisigTransaction = new MultisigTransaction(timeStamp, senderAccount, transaction);
-        multisigTransaction.setFee(feeCalculator.calculateMinimumFee(multisigTransaction));
+        multisigTransaction.setFee(utilityFunc.TransactionFeeCalculator.calculateMinimumFee(multisigTransaction));
         multisigTransaction.setDeadline(timeStamp.addHours(23));
         multisigTransaction.sign();
 
@@ -288,9 +309,7 @@ public class SimplifyNEM {
         Account multisigAccount = new Account(Address.fromEncoded(multiSigAddress));
 
         MultisigSignatureTransaction transaction = new MultisigSignatureTransaction(timeStamp, coSigAcc, multisigAccount, Hash.fromHexString(transactionHash));
-        //572500, 975000 Testnet Fee Fork Heights
-        //875000, 1110000 Mainnet Fee Fork Heights
-        transaction.setFee(new DefaultTransactionFeeCalculator(utilityFunc, () -> new BlockHeight(utilityFunc.getBlockHeight()), new BlockHeight(975000)).calculateMinimumFee(transaction));
+        transaction.setFee(utilityFunc.TransactionFeeCalculator.calculateMinimumFee(transaction));
         transaction.setDeadline(timeStamp.addHours(23));
         transaction.sign();
 
